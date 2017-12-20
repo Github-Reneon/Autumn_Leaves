aaed12d49eb33dc54a3f8c17f5f3f5ea8b4ccac6
Github-Reneon <andrewlpriv@gmail.com>
2017-12-20 15:02:08 +0000

    I forgot all the changes I made. Game manager i guess

--- main.cpp
+++ main.cpp
@@ -1,72 +1,13 @@
+#include "gamemanager.h"
 
-#include "charactercontroller.h"
-sf::RenderWindow window(sf::VideoMode(512,512), "Autumn Leaves", sf::Style::Close | sf::Style::Titlebar);
+gamemanager manager;
+objects gold("Gold", "coins", 4.0f, 4.0f);
 
-characterController player;
-int main(int argc, char** argv)
+int main()
 {
-
-    float xPosChange = 0.0f;
-    float yPosChange = 0.0f;
-    float xNegChange = 0.0f;
-    float yNegChange = 0.0f;
-    float spd = 0.2f;
-    while(window.isOpen()){
-    sf::Event event;
-        while(window.pollEvent(event)){
-            switch(event.type){
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                case sf::Event::EventType::KeyPressed:
-                    switch (event.key.code){
-                        case sf::Keyboard::W:
-                            yNegChange = spd;
-                            break;
-                        case sf::Keyboard::A:
-                            xNegChange = spd;
-                            break;
-                        case sf::Keyboard::S:
-                            yPosChange = spd;
-                            break;
-                        case sf::Keyboard::D:
-                            xPosChange = spd;
-                            break;
-                        case sf::Keyboard::Escape:
-                            window.close();
-                            break;
-                    }
-                    break;
-                case sf::Event::EventType::KeyReleased:
-                    switch (event.key.code) {
-                        case sf::Keyboard::W:
-                            yNegChange = 0;
-                            break;
-                        case sf::Keyboard::A:
-                            xNegChange = 0;
-                            break;
-                        case sf::Keyboard::S:
-                            yPosChange = 0;
-                            break;
-                        case sf::Keyboard::D:
-                            xPosChange = 0;
-                            break;
-                    }
-            }
-        }
-        player.x += xPosChange - xNegChange;
-        player.y += yPosChange - yNegChange;
-        //----Game Loop----------///
-        //Clearing the previous screen.
-        player.setCords();
-        window.clear(sf::Color::Black);
-
-        //Drawing happens here
-        window.draw(player.sprite);
-
-        //Displaying a new one
-        window.display();
-        //----End of Game Loop---///
+    while(manager.window.isOpen())
+    {
+        manager.loop();
     }
 
     return 0;
